e073adc557a68d60eee46bb044b42b2045593b5c
Hansrenee Willysandro <hansrenee>
2019-05-04 18:50:36 +0700

    updated database name

--- conn.php
+++ conn.php
@@ -2,7 +2,7 @@
 $db_host = "localhost";
 $db_user = "root";
 $db_pass = "";
-$db_name = "distroit";
+$db_name = "bookingcerdas";
 
 $koneksi = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
